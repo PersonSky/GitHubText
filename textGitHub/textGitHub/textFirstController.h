970c73a1d7fb9c6b7fb147b428e65a36cdd1ede1
吴鸥 <wuou@wuoudemacbook-air.local>
2019-01-12 18:07:05 +0800

    添加了一个控制器

---
+++ textGitHub/textGitHub/textFirstController.h
@@ -0,0 +1,13 @@
+//
+//  textFirstController.h
+//  textGitHub
+//
+//  Created by 吴鸥 on 2019/1/12.
+//  Copyright © 2019年 台州橙云科技网络有限公司. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface textFirstController : UIViewController
+
+@end
---
+++ textGitHub/textGitHub/textFirstController.m
@@ -0,0 +1,37 @@
+//
+//  textFirstController.m
+//  textGitHub
+//
+//  Created by 吴鸥 on 2019/1/12.
+//  Copyright © 2019年 台州橙云科技网络有限公司. All rights reserved.
+//
+
+#import "textFirstController.h"
+
+@interface textFirstController ()
+
+@end
+
+@implementation textFirstController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
